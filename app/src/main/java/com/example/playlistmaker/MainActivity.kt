6ee978a95a1842dff18d692635619817caa9cc72
Yuriy Chistov <1350077@mail.ru>
2024-11-29 22:13:28 +0300

    Сделаны кнопки, добавлены "Тосты"

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +14,28 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btn_1 = findViewById<FrameLayout>(R.id.btn_search)
+
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку ПОИСК", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btn_1.setOnClickListener(imageClickListener)
+
+        val btn_2 = findViewById<FrameLayout>(R.id.btn_media)
+
+        btn_2.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку МЕДИАТЕКА", Toast.LENGTH_SHORT).show()
+        }
+
+        val btn_3 = findViewById<FrameLayout>(R.id.btn_settings)
+
+        btn_3.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку НАСТРОЙКИ", Toast.LENGTH_SHORT).show()
         }
     }
 }
