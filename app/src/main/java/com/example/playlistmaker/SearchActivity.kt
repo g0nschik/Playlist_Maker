30cd4f26832b9d97492830e5cf1905da23522b13
Yuriy Chistov <1350077@mail.ru>
2025-01-31 01:09:46 +0300

    Домашнее задание - Спринт №11

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,20 +1,29 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
     private var searchTextValue: CharSequence? = TEXT_DEF
@@ -22,11 +31,23 @@ class SearchActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var trackAdapter: TrackAdapter
     private lateinit var trackList: ArrayList<Track>
+    private lateinit var placeholder: LinearLayout
+
+    private val baseUrl = "https://itunes.apple.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val trackService = retrofit.create(TrackService::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_search)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -34,20 +55,40 @@ class SearchActivity : AppCompatActivity() {
         }
 
         val backButton  = findViewById<FrameLayout>(R.id.btn_back)
+        val searchLine = findViewById<EditText>(R.id.inputSearchText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val updateButton = findViewById<ImageView>(R.id.updateButton)
+        
+        placeholder = findViewById<LinearLayout>(R.id.placeholderLayout)
+
+        searchLine.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                hideInput()
+                showMessage("loading")
+                performSearch(searchLine.text.toString())
+                true
+            } else {
+                false
+            }
+        }
 
         backButton.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
         }
 
-        val searchLine = findViewById<EditText>(R.id.inputSearchText)
-        val clearButton = findViewById<ImageView>(R.id.clearIcon)
-
         clearButton.setOnClickListener {
-            searchLine.setText("")
-            val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(searchLine.windowToken, 0)
+            searchLine.setText(TEXT_DEF)
+            hideInput()
             searchLine.clearFocus()
+            trackList.clear()
+            recyclerView.visibility = View.GONE
+            showMessage("hide")
+        }
+
+        updateButton.setOnClickListener {
+            showMessage("loading")
+            performSearch(searchLine.text.toString())
         }
 
         val simpleTextWatcher = object : TextWatcher {
@@ -72,19 +113,72 @@ class SearchActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.itemsList)
 
-        // Список треков - "мок-объект"
-        val trackList = mutableListOf(
-            Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
-        )
-        // ---------------------------
-
+        trackList = ArrayList()
         trackAdapter = TrackAdapter(trackList)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = trackAdapter
+    }
+
+    private fun hideInput() {
+        val searchLine = findViewById<EditText>(R.id.inputSearchText)
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(searchLine.windowToken, 0)
+    }
+
+    private fun performSearch(query: String) {
+        trackService.search(query).enqueue(object : Callback<TrackResponse> {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onResponse(call: Call<TrackResponse>, response: Response<TrackResponse>) {
+                placeholder.visibility = View.GONE
+                if (response.isSuccessful) {
+                    trackList.clear()
+                    if (response.body()?.results?.isNotEmpty() == true) {
+                        trackList.addAll(response.body()?.results!!)
+                        trackAdapter.notifyDataSetChanged()
+                        recyclerView.visibility = View.VISIBLE
+                        showMessage("hide")
+                    } else {
+                        showMessage("empty")
+                    }
+                } else {
+                    showMessage("error")
+                }
+            }
+
+            override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                showMessage("error")
+            }
+        })
+    }
+
+    private fun showMessage(type: String) {
+        val messageImage: ImageView = placeholder.findViewById(R.id.placeholderImage)
+        val messageText: TextView = placeholder.findViewById(R.id.placeholderText)
+        val updateButton: ImageView = placeholder.findViewById(R.id.updateButton)
+
+        placeholder.visibility = View.GONE
+        updateButton.visibility = View.GONE
+
+        when (type) {
+            "loading" -> {
+                messageImage.visibility = View.GONE
+                messageText.text = getString(R.string.loading)
+                placeholder.visibility = View.VISIBLE
+            }
+            "empty" -> {
+                messageImage.setImageResource(R.drawable.no_results)
+                messageImage.visibility = View.VISIBLE
+                messageText.text = getString(R.string.no_results)
+                placeholder.visibility = View.VISIBLE
+            }
+            "error" -> {
+                messageImage.setImageResource(R.drawable.net_error)
+                messageImage.visibility = View.VISIBLE
+                messageText.text = getString(R.string.net_error)
+                updateButton.visibility = View.VISIBLE
+                placeholder.visibility = View.VISIBLE
+            }
+        }
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -108,13 +202,5 @@ class SearchActivity : AppCompatActivity() {
     companion object {
         const val SEARCH_TEXT_VALUE = "SEARCH_TEXT_VALUE"
         const val TEXT_DEF = ""
-    }
-
-    private fun myTrackList() {
-        trackList.add(Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"))
-        trackList.add(Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"))
-        trackList.add(Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"))
-        trackList.add(Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"))
-        trackList.add(Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"))
     }
 }
